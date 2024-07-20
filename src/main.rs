76b838ad0a329b56b6ae0a387c5724990595ad5b
arthomnix <arthomnix@arthomnix.dev>
2024-07-20 13:26:02 +0100

    Add github link to user agent

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ fn main() {
     let gid = uuid::Uuid::new_v4().to_string();
     debug!("gid {gid}");
     let client = reqwest::blocking::Client::builder()
-        .user_agent("wbr-cli/0.1.0")
+        .user_agent("wbr-cli/0.1.0 (+https://github.com/arthomnix/wbr-cli)")
         .build()
         .unwrap();
 
